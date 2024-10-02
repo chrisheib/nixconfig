2afff6cf39e986f14857f11e8feec1e0b2435627
STSchiff <6112968+chrisheib@users.noreply.github.com>
2024-10-02 17:34:37 +0200

    mostly working

---
+++ software.nix
@@ -0,0 +1,43 @@
+{ config, pkgs, ... }:
+{
+  boot.kernelPackages = pkgs.linuxPackages_6_10; 
+
+  programs.bash.shellAliases = {
+    l = "ls -alh";
+    ll = "ls -l";
+    ls = "ls --color=tty";
+    nrs = "sudo nix-channel --update && sudo nixos-rebuild switch -I nixos-config=/home/schiff/nixconfig/configuration.nix";
+    nrsrepair = "sudo nixos-rebuild switch --repair -I nixos-config=/home/schiff/nixconfig/configuration.nix";
+  };
+
+  boot.kernelModules = ["88XXau"];
+  boot.extraModulePackages = with config.boot.kernelPackages; [
+    rtl88xxau-aircrack
+  ];
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget  
+  environment.systemPackages = with pkgs; [
+    git
+    chromium
+    usbutils    
+    lshw
+    (vscode-with-extensions.override {
+      vscode = vscodium;
+      vscodeExtensions = with vscode-extensions; [
+        bbenoist.nix
+        ms-python.python
+        ms-azuretools.vscode-docker
+        ms-vscode-remote.remote-ssh
+        jnoortheen.nix-ide
+      ] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+        {
+          name = "remote-ssh-edit";
+          publisher = "ms-vscode-remote";
+          version = "0.47.2";
+          sha256 = "1hp6gjh4xp2m1xlm1jsdzxw9d8frkiidhph6nvl24d0h8z34w49g";
+        }
+      ];
+    })
+  ];
+}
