25e22acc3deb63837edcff75df6583de42c50069
STSchiff <6112968+chrisheib@users.noreply.github.com>
2024-10-08 02:20:33 +0200

    Generation 40

--- nvidia.nix
+++ nvidia.nix
@@ -47,13 +47,32 @@
     ];
   };
 
+  nixpkgs.config.packageOverrides = pkgs: {inherit (pkgs) linuxPackages_latest nvidia_x11;};
+
   hardware.nvidia = {
     modesetting.enable = true;
     powerManagement.enable = true;
     open = false;
-    nvidiaSettings = true;
-    package = config.boot.kernelPackages.nvidiaPackages.stable;
+    nvidiaSettings = false;
+    # package = config.boot.kernelPackages.nvidiaPackages.beta;
+
+    # Vulkan errors...
+    package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
+      version = "560.35.03";
+      sha256_64bit = "sha256-8pMskvrdQ8WyNBvkU/xPc/CtcYXCa7ekP73oGuKfH+M=";
+      sha256_aarch64 = "sha256-s8ZAVKvRNXpjxRYqM3E5oss5FdqW+tv1qQC2pDjfG+s=";
+      openSha256 = "sha256-/32Zf0dKrofTmPZ3Ratw4vDM7B+OgpC4p7s+RHUjCrg=";
+      settingsSha256 = "sha256-kQsvDgnxis9ANFmwIwB7HX5MkIAcpEEAHc8IBOLdXvk=";
+      persistencedSha256 = "sha256-E2J2wYYyRu7Kc3MMZz/8ZIemcZg68rkzvqEwFAL3fFs=";
+    };
   };
+
+  services.xserver.videoDrivers = ["nvidia"];
+
+  boot.kernelParams = [
+    "nvidia-drm.modeset=1"
+    "nvidia-drm.fbdev=1"
+  ];
 }
 #  # Load nvidia driver for Xorg and Wayland
 #  # breaks everything :(
