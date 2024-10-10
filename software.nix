2d67985969f036e1efe66df7b62bd6b61a2de187
STSchiff <6112968+chrisheib@users.noreply.github.com>
2024-10-10 18:21:23 +0200

    Generation 47

--- software.nix
+++ software.nix
@@ -25,6 +25,8 @@
     rtl88xxau-aircrack
   ];
 
+  programs.kdeconnect.enable = true;
+
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
