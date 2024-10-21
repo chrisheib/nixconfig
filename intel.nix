1de69ecea129d13f3887ea4c537cee25cc362f59
STSchiff <6112968+chrisheib@users.noreply.github.com>
2024-10-21 02:56:29 +0200

    Generation 49

--- intel.nix
+++ intel.nix
@@ -7,9 +7,13 @@
   hardware.opengl = {
     enable = true;
     extraPackages = with pkgs; [
+      vaapiIntel
       intel-media-driver # For Broadwell (2014) or newer processors. LIBVA_DRIVER_NAME=iHD
       intel-vaapi-driver # For older processors. LIBVA_DRIVER_NAME=i965
     ];
   };
-  environment.sessionVariables = {LIBVA_DRIVER_NAME = "iHD";}; # Optionally, set the environment variable
+  environment.sessionVariables = {
+    LIBVA_DRIVER_NAME = "iHD";
+    VDPAU_DRIVER = "va_gl";
+  }; # Optionally, set the environment variable
 }
