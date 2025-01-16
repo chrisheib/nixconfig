6660e6cc55c67260b69a62156b681b35085241b3
STSchiff <6112968+chrisheib@users.noreply.github.com>
2025-01-16 23:15:01 +0100

    Generation 30

--- configuration.nix
+++ configuration.nix
@@ -32,13 +32,13 @@
       ];
     plymouth = {
       enable = true;
-      theme = "rings";
-      themePackages = with pkgs; [
-        # By default we would install all themes
-        (adi1090x-plymouth-themes.override {
-          selected_themes = ["rings"];
-        })
-      ];
+      # theme = "rings";
+      # themePackages = with pkgs; [
+      #   # By default we would install all themes
+      #   (adi1090x-plymouth-themes.override {
+      #     selected_themes = ["rings"];
+      #   })
+      # ];
     };
 
     # Enable "Silent Boot"
