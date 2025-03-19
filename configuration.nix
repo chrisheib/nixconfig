bc9f1720e7e6cea5e29e05b485cb37296ccb8b44
STSchiff <6112968+chrisheib@users.noreply.github.com>
2025-03-19 19:49:39 +0100

    Generation 120: update nvidia driver to 570.133.07

--- configuration.nix
+++ configuration.nix
@@ -261,10 +261,10 @@ in {
   # services.xserver.videoDrivers = ["nvidia" "intel"];
   hardware.nvidia = {
     package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
-      version = "570.124.04"; # use new 570 drivers
-      sha256_64bit = "sha256-G3hqS3Ei18QhbFiuQAdoik93jBlsFI2RkWOBXuENU8Q=";
-      openSha256 = "sha256-KCGUyu/XtmgcBqJ8NLw/iXlaqB9/exg51KFx0Ta5ip0=";
-      settingsSha256 = "sha256-LNL0J/sYHD8vagkV1w8tb52gMtzj/F0QmJTV1cMaso8=";
+      version = "570.133.07"; # use new 570 drivers
+      sha256_64bit = "sha256-LUPmTFgb5e9VTemIixqpADfvbUX1QoTT2dztwI3E3CY="; # "sha256-XMk+FvTlGpMquM8aE8kgYK2PIEszUZD2+Zmj2OpYrzU="; # .run.drv
+      openSha256 = "sha256-9l8N83Spj0MccA8+8R1uqiXBS0Ag4JrLPjrU3TaXHnM=";
+      settingsSha256 = "sha256-XMk+FvTlGpMquM8aE8kgYK2PIEszUZD2+Zmj2OpYrzU="; # src.drv
       usePersistenced = false;
     };
 
