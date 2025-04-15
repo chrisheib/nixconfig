d0c18c9700581285b97adab337563928a20d59d4
STSchiff <6112968+chrisheib@users.noreply.github.com>
2025-04-15 12:34:43 +0200

    Generation 61

--- software.nix
+++ software.nix
@@ -5,7 +5,7 @@
 }: {
   # boot.kernelPackages = pkgs.linuxPackages_6_10;
   # boot.kernelPackages = pkgs.linuxPackages_6_9;
-  boot.kernelPackages = pkgs.linuxPackages_xanmod_stable;
+  # boot.kernelPackages = pkgs.linuxPackages_xanmod_stable;
 
   programs.bash.shellAliases = {
     l = "ls -alh";
@@ -26,6 +26,8 @@
   boot.extraModulePackages = with config.boot.kernelPackages; [
     rtl88xxau-aircrack
   ];
+  boot.plymouth.enable = true;
+  boot.loader.timeout = 1;
 
   nix.extraOptions = ''
     experimental-features = nix-command
@@ -63,23 +65,14 @@
     lshw
     (vscode-with-extensions.override {
       # vscode = vscodium;
-      vscodeExtensions = with vscode-extensions;
-        [
-          bbenoist.nix
-          ms-python.python
-          ms-azuretools.vscode-docker
-          ms-vscode-remote.remote-ssh
-          jnoortheen.nix-ide
-          kamadorueda.alejandra
-        ]
-        ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
-          {
-            name = "remote-ssh-edit";
-            publisher = "ms-vscode-remote";
-            version = "0.47.2";
-            sha256 = "1hp6gjh4xp2m1xlm1jsdzxw9d8frkiidhph6nvl24d0h8z34w49g";
-          }
-        ];
+      vscodeExtensions = with vscode-extensions; [
+        bbenoist.nix
+        ms-python.python
+        ms-azuretools.vscode-docker
+        ms-vscode-remote.remote-ssh
+        jnoortheen.nix-ide
+        kamadorueda.alejandra
+      ];
     })
   ];
 }
