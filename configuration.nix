c0cfa8ff5e4203a0cd1bb4b9f59d2ffc30c7d417
STSchiff <6112968+chrisheib@users.noreply.github.com>
2025-04-15 23:48:55 +0200

    Generation 139: change steam to unstable

--- configuration.nix
+++ configuration.nix
@@ -432,13 +432,15 @@ in {
 
     geekbench
 
+    unstable.steam
+
     variety # wallpaper changer
   ];
 
   # linuxKernel.packages.linux_xanmod_latest.turbostat.enable = true;
 
   programs.steam = {
-    enable = true;
+    # enable = true;
     remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
     dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
     localNetworkGameTransfers.openFirewall = true;
