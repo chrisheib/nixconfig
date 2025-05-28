8a2ff0ea3fbc7f12477ad03294afd5a1d9fd7f78
STSchiff <6112968+chrisheib@users.noreply.github.com>
2025-05-28 22:37:32 +0200

    Generation  159   2025-05-20 02:22:10   (current): change power governor

--- hardware-configuration.nix
+++ hardware-configuration.nix
@@ -12,6 +12,8 @@
     (modulesPath + "/installer/scan/not-detected.nix")
   ];
 
+  powerManagement.cpuFreqGovernor = "performance";
+
   boot.initrd.availableKernelModules = ["xhci_pci" "ahci" "nvme" "usbhid" "usb_storage" "sd_mod"];
   boot.initrd.kernelModules = [];
   boot.kernelModules = ["kvm-intel"];
