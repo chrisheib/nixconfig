cb03e0a10236b9f3c74eab51fd1ffec86d2ab59f
STSchiff <6112968+chrisheib@users.noreply.github.com>
2025-06-09 20:25:38 +0200

    Generation 173: reenable swap

--- hardware-configuration.nix
+++ hardware-configuration.nix
@@ -49,6 +49,10 @@
     options = ["rw" "uid=1000" "nofail"];
   };
 
+  swapDevices = [
+    {device = "/dev/disk/by-uuid/2d1cca01-26a6-4f29-bccf-d5e472751981";}
+  ];
+
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
   # still possible to use this option, but it's recommended to use it in conjunction
