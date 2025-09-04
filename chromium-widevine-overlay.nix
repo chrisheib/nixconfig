5c5f8ea4a9df2d9f7287cdfbac0e7f78c48604f8
STSchiff <6112968+chrisheib@users.noreply.github.com>
2025-09-04 02:54:25 +0200

    Generation 272: more chromium

--- chromium-widevine-overlay.nix
+++ chromium-widevine-overlay.nix
@@ -9,14 +9,16 @@
         ungoogled-chromium = super.ungoogled-chromium.overrideAttrs (oldAttrs: {
           # ensure we reference widevine so it becomes a dependency
           buildInputs = (oldAttrs.buildInputs or [ ]) ++ [ widevine ];
+          proprietaryCodecs = true;
+          enableWidevine = true;
           postInstall = ''
             ${oldAttrs.postInstall or ""}
-            mkdir -p $out/share/ungoogled-chromium
+            mkdir -p $out/share/chromium
             # copy any WidevineCdm* directory from the widevine package
-            cp -R "${widevine}/WidevineCdm" "$out/share/ungoogled-chromium/WidevineCdm/4.10.2891.0"
+            cp -R "${widevine}/WidevineCdm" "$out/share/chromium/WidevineCdm/4.10.2891.0"
 
             # create the "latest" pointer file Chromium expects
-            printf '{"Path":"%s"}\n' "$out/share/ungoogled-chromium/WidevineCdm/4.10.2891.0" > $out/share/ungoogled-chromium/latest-component-updated-widevine-cdm
+            printf '{"Path":"%s"}\n' "$out/share/chromium/WidevineCdm/4.10.2891.0" > $out/share/chromium/latest-component-updated-widevine-cdm
           '';
         });
       }
