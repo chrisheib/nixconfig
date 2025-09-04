dd7b694c0046f3202f8465485b40d0b249178aaf
STSchiff <6112968+chrisheib@users.noreply.github.com>
2025-09-04 03:22:29 +0200

    Generation 273: more chromium

--- chromium-widevine-overlay.nix
+++ chromium-widevine-overlay.nix
@@ -11,14 +11,16 @@
           buildInputs = (oldAttrs.buildInputs or [ ]) ++ [ widevine ];
           proprietaryCodecs = true;
           enableWidevine = true;
+          # share/google/chrome/WidevineCdm/_platform_specific/linux_x64/libwidevinecdm.so
           postInstall = ''
             ${oldAttrs.postInstall or ""}
-            mkdir -p $out/share/chromium
+            cp ${widevine}/share/google/chrome/WidevineCdm/_platform_specific/linux_x64/libwidevinecdm.so $out/share/chromium/libwidevinecdm.so
+            # mkdir -p $out/share/chromium
             # copy any WidevineCdm* directory from the widevine package
-            cp -R "${widevine}/WidevineCdm" "$out/share/chromium/WidevineCdm/4.10.2891.0"
+            # cp -R "${widevine}/WidevineCdm" "$out/share/chromium/WidevineCdm/4.10.2891.0"
 
             # create the "latest" pointer file Chromium expects
-            printf '{"Path":"%s"}\n' "$out/share/chromium/WidevineCdm/4.10.2891.0" > $out/share/chromium/latest-component-updated-widevine-cdm
+            # printf '{"Path":"%s"}\n' "$out/share/chromium/WidevineCdm/4.10.2891.0" > $out/share/chromium/latest-component-updated-widevine-cdm
           '';
         });
       }
