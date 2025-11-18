b1de088a87e9e6baaade4f90818e2f2cfd13d057
STSchiff <6112968+chrisheib@users.noreply.github.com>
2025-11-19 00:31:41 +0100

    Prepare

--- configuration.nix
+++ configuration.nix
@@ -801,6 +801,12 @@ in
     };
   };
 
+  services.ollama = {
+    enable = true;
+    acceleration = "cuda";
+    loadModels = [ "qwen3-embedding:0.6b" ];
+  };
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
