6df4e85366eb3b84d076cf541d29b4cb9f365bd1
STSchiff <6112968+chrisheib@users.noreply.github.com>
2025-11-26 19:58:17 +0100

    Generation 318: update

--- configuration.nix
+++ configuration.nix
@@ -620,7 +620,7 @@ in
     # cava # audio visualizer
     # qt6.qtwebsockets
     (callPackage /home/stschiff/projects/nixpkgs/pkgs/by-name/ku/kurve/package.nix { })
-    # (callPackage /etc/nixos/tempderivation.nix { })
+    (callPackage /etc/nixos/tempderivation.nix { })
     # plasmusic-toolbar
     # kurve
 
--- tempderivation.nix
+++ tempderivation.nix
@@ -1,31 +1,35 @@
 {
   lib,
-  stdenvNoCC,
-  fetchFromGitHub,
-  kdePackages,
+  appimageTools,
+  fetchurl,
 }:
 
-stdenvNoCC.mkDerivation {
-  pname = "shutdown_or_switch";
-  version = "unstable-2025-09-29";
+let
+  version = "0.12.8";
+  pname = "Exiled-Exchange-2";
 
-  src = fetchFromGitHub {
-    owner = "Davide-sd";
-    repo = "shutdown_or_switch";
-    rev = "master";
-    sha256 = "sha256-NbZyQu23gr0O1QHbhLbcaTXV6t639akW9PF0Jq4Sc3Y=";
+  src = fetchurl {
+    url = "https://github.com/Kvan7/Exiled-Exchange-2/releases/download/v${version}/Exiled-Exchange-2-${version}.AppImage";
+    hash = "sha256-hGUmwyhFsM+8XTrFCuaLVYAA85jwrKCftkQ/wlViRHI=";
   };
 
-  installPhase = ''
-    mkdir -p $out/share/plasma/plasmoids/
-    cp -r $src/package $out/share/plasma/plasmoids/org.kde.plasma.shutdownorswitch
-  '';
+  # appimageContents = appimageTools.extract { inherit pname version src; };
+in
+appimageTools.wrapType2 rec {
+  inherit pname version src;
 
-  meta = with lib; {
-    homepage = "https://github.com/Davide-sd/shutdown_or_switch";
-    description = "KDE Plasma widget to quickly access the shutdown options or switch user.";
-    license = lib.licenses.gpl2Only;
-    maintainers = with lib.maintainers; [ Svenum ];
-    inherit (kdePackages.kwindowsystem.meta) platforms;
+  # extraInstallCommands = ''
+  #   substituteInPlace $out/share/applications/${pname}.desktop \
+  #     --replace-fail 'Exec=AppRun' 'Exec=${meta.mainProgram}'
+  # '';
+
+  meta = {
+    description = "Path of Exile 2 trading app for price checking";
+    homepage = "https://kvan7.github.io/Exiled-Exchange-2/quick-start";
+    downloadPage = "https://github.com/Kvan7/Exiled-Exchange-2/releases";
+    license = lib.licenses.mit;
+    sourceProvenance = with lib.sourceTypes; [ binaryNativeCode ];
+    maintainers = with lib.maintainers; [ chrisheib ];
+    platforms = [ "x86_64-linux" ];
   };
 }
