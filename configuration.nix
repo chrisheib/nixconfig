6bb5ce4d11500a13c67535829e520e9cf15b01b0
STSchiff <6112968+chrisheib@users.noreply.github.com>
2025-11-30 12:56:08 +0100

    Generation 328: fix flake warning, add chaotic

--- configuration.nix
+++ configuration.nix
@@ -54,7 +54,8 @@ in
     loader.systemd-boot.configurationLimit = 5;
     loader.efi.canTouchEfiVariables = true;
     # cachyos: https://www.nyx.chaotic.cx/
-    kernelPackages = pkgs.linuxPackages_xanmod_latest;
+    # kernelPackages = pkgs.linuxPackages_xanmod_latest;
+    kernelPackages = pkgs.linuxPackages_cachyos-lto;
     extraModprobeConfig =
       "options nvidia "
       + lib.concatStringsSep " " [
