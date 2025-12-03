dde89b6df9a45986eeaa5aa68993d24caa18f862
STSchiff <6112968+chrisheib@users.noreply.github.com>
2025-12-03 01:26:18 +0100

    Generation 330: enable ccache for ollama, nvidia, cachykernel

--- configuration.nix
+++ configuration.nix
@@ -27,6 +27,31 @@ in
     config = import ./nixpkgs-config.nix;
     overlays = [
       # (import /etc/nixos/overlays/plain-pkgs.nix)
+      (self: super: {
+        ccacheWrapper = super.ccacheWrapper.override {
+          extraConfig = ''
+            export CCACHE_COMPRESS=1
+            export CCACHE_DIR="${config.programs.ccache.cacheDir}"
+            export CCACHE_UMASK=007
+            if [ ! -d "$CCACHE_DIR" ]; then
+              echo "====="
+              echo "Directory '$CCACHE_DIR' does not exist"
+              echo "Please create it with:"
+              echo "  sudo mkdir -m0770 '$CCACHE_DIR'"
+              echo "  sudo chown root:nixbld '$CCACHE_DIR'"
+              echo "====="
+              exit 1
+            fi
+            if [ ! -w "$CCACHE_DIR" ]; then
+              echo "====="
+              echo "Directory '$CCACHE_DIR' is not accessible for user $(whoami)"
+              echo "Please verify its access permissions"
+              echo "====="
+              exit 1
+            fi
+          '';
+        };
+      })
     ];
   };
 
@@ -286,6 +311,7 @@ in
       ];
       cores = 24; # threads per build job https://search.nixos.org/options?channel=unstable&show=nix.settings.cores&query=nix.settings
       max-jobs = 4; # parallel build jobs https://search.nixos.org/options?channel=unstable&show=nix.settings.max-jobs&query=nix.settings
+      extra-sandbox-paths = [ config.programs.ccache.cacheDir ];
     };
     # nixPath = [
     # "nixpkgs=/home/stschiff/projects/nixpkgs"
@@ -293,6 +319,13 @@ in
     # "nixos-config=/etc/nixos/configuration.nix"
     # ];
   };
+
+  programs.ccache.enable = true;
+  programs.ccache.packageNames = [
+    "ollama"
+    "nvidia-driver"
+    "linux-cachyos-lto"
+  ];
 
   programs.partition-manager.enable = true;
 
