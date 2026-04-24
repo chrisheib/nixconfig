2c15214890f48cf4e93ac653be0ea477dbef3404
STSchiff <6112968+chrisheib@users.noreply.github.com>
2026-04-25 00:27:25 +0200

    Prepare: edid

--- configuration.nix
+++ configuration.nix
@@ -118,7 +118,8 @@
       # IOMMU for GPU passthrough
       "amd_iommu=on"
       "iommu=pt"
-      "drm.edid_firmware=DP-2:edid/EDID_144hz.bin"
+      "drm.edid_firmware=DP-2:edid/EDID_60hz.bin"
+      "video=DP-2:1920x1080@120e"
       # "vfio-pci.ids=1002:13c0,1002:1640"
     ];
 
