bb7a87a22fd39b8388bd11ab67800defcd4ee0aa
STSchiff <6112968+chrisheib@users.noreply.github.com>
2026-05-10 23:46:13 +0200

    Prepare: ollama

--- configuration.nix
+++ configuration.nix
@@ -955,7 +955,7 @@
     package = pkgs.ollama-rocm;
     # Wichtig: Explizites Target für RDNA4 (9070 XT)
     # rocmPackages = pkgs.rocmPackages_sdk;
-    environment = {
+    environmentVariables = {
       OLLAMA_FLASH_ATTENTION = "1"; # Enable Flash Attention for better performance on RDNA4 GPUs
     };
   };
